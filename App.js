708cb8e64cf223652d98272e1106ce6e24245412
Yongmin <qwelllo@naver.com>
2023-09-17 22:54:54 +0900

    [4.2] Animation 비효율적으로 체험

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components/native';
+// npm install @types/styled-components @types/styled-components-react-native
+
+const Container = styled.View`
+  flex : 1;
+  align-items: center;
+  justify-content: center;
+`;
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`
 
 export default function App() {
+  const [y, setY] = useState(0);
+  const [intervalID, setIntervalID] = useState(0);
+  const moveUp = () => {
+    const id = setInterval(() => {
+      setY(prev => prev + 1);
+    }, 5);
+
+    setIntervalID(id);
+  };
+
+  useEffect(()=> {
+    if(y >= 200) {
+      clearInterval(intervalID);
+    }
+  },[y, intervalID])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Box style={{
+        transform: [{translateY: y}]
+      }} onPress={moveUp}></Box>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
