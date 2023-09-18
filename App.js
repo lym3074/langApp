fd5c7905fbd5d93a59bab0c61732a4b05c150dc7
Yongmin <qwelllo@naver.com>
2023-09-18 20:37:25 +0900

    [4.7] interpolate의 다양한 기능 확인, ValueXY 변경

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ const Box = styled.View`
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const Y_POSITION = useRef(new Animated.Value(300)).current;
+  const POSITION = useRef(new Animated.ValueXY({x: 0, y: 300})).current;
   // useRef는 lifeTime동안 object를 지속시켜 준다.
   // 렌더링 시 값이 초기화되는 것을 막아준다.
   const [up, setUp] = useState(false);
@@ -25,28 +25,32 @@ export default function App() {
   const toggleUp = () => setUp(prev => !prev);
 
   const moveUp = () => {
-    Animated.timing(Y_POSITION, {
+    Animated.timing(POSITION, {
       toValue: up? 300 : -300,
       useNativeDriver: true,
       duration: 1000
     }).start(toggleUp); // end될 때의 콜백함수
   }
+  const rotateY = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["-360deg", "360deg"]
+  })
 
-  const opacity = Y_POSITION.interpolate({
-    inputRange: [-300, 0, 300], // 대상에서 어떤 값을 갖고 올 것인지 정의
-    outputRange : [1, 0.5, 1] // 반영될 값
-  });
-
-  const borderRadius = Y_POSITION.interpolate({
+  const borderRadius = POSITION.y.interpolate({
     inputRange: [-300, 300], // 대상에서 어떤 값을 갖고 올 것인지 정의
     outputRange : [100, 0]
   });
+
+  const bgColor = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"]
+  })
 
   return (
     <Container>
       <Pressable onPress={moveUp}>
         <AnimatedBox
-          style={{transform: [{translateY: Y_POSITION}], opacity, borderRadius}}
+          style={{transform: [{rotateY},{translateY: POSITION.y}], borderRadius, backgroundColor: bgColor}}
         />
       </Pressable>
       
