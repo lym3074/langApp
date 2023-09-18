b960140e58bcedd688aadfd41f5473a7dc8f4a4e
Yongmin <qwelllo@naver.com>
2023-09-18 20:13:10 +0900

    [4.6] 도형 opacity, 모양 변경(interpolate 이용)

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Animated, Easing, TouchableOpacity } from 'react-native';
+import { Animated, Easing, Pressable, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 // npm install @types/styled-components @types/styled-components-react-native
 
@@ -17,7 +17,7 @@ const Box = styled.View`
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const Y = useRef(new Animated.Value(0)).current;
+  const Y_POSITION = useRef(new Animated.Value(300)).current;
   // useRef는 lifeTime동안 object를 지속시켜 준다.
   // 렌더링 시 값이 초기화되는 것을 막아준다.
   const [up, setUp] = useState(false);
@@ -25,20 +25,30 @@ export default function App() {
   const toggleUp = () => setUp(prev => !prev);
 
   const moveUp = () => {
-    Animated.timing(Y, {
-      toValue: up? 200 : -200,
+    Animated.timing(Y_POSITION, {
+      toValue: up? 300 : -300,
       useNativeDriver: true,
-      easing: Easing.circle
+      duration: 1000
     }).start(toggleUp); // end될 때의 콜백함수
   }
 
+  const opacity = Y_POSITION.interpolate({
+    inputRange: [-300, 0, 300], // 대상에서 어떤 값을 갖고 올 것인지 정의
+    outputRange : [1, 0.5, 1] // 반영될 값
+  });
+
+  const borderRadius = Y_POSITION.interpolate({
+    inputRange: [-300, 300], // 대상에서 어떤 값을 갖고 올 것인지 정의
+    outputRange : [100, 0]
+  });
+
   return (
     <Container>
-      <TouchableOpacity onPress={moveUp}>
+      <Pressable onPress={moveUp}>
         <AnimatedBox
-          style={{transform: [{translateY: Y}]}}
+          style={{transform: [{translateY: Y_POSITION}], opacity, borderRadius}}
         />
-      </TouchableOpacity>
+      </Pressable>
       
     </Container>
   );
@@ -51,3 +61,5 @@ export default function App() {
 // animated.decay() : 객체나 느리게 멈추는 기능
 // animated.spring() : 물리 모델 제공
 // animated.timing() : 타이밍
+
+// interpolation : 보간 -> 중간값 추정
