7234bf56599afbab3fffc90f95c011b9e844d491
Yongmin <qwelllo@naver.com>
2023-09-20 20:31:40 +0900

    [4.13] x좌표 이동 카드 생성(병렬 애니메이션)

--- App.js
+++ App.js
@@ -1,63 +1,69 @@
 import React, {useRef } from 'react';
-import { Animated, PanResponder} from 'react-native';
+import { Animated, PanResponder, View} from 'react-native';
 import styled from 'styled-components/native';
+import {Ionicons} from "@expo/vector-icons";
 // npm install @types/styled-components @types/styled-components-react-native
 
 const Container = styled.View`
   flex : 1;
   align-items: center;
   justify-content: center;
+  background-color: #00a8ff;
 `;
-const Box = styled.View`
-  background-color: tomato;
+
+const Card = styled(Animated.createAnimatedComponent(View))`
+  background-color:white;
   width: 200px;
   height: 200px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 12px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.3);
 `;
 
-const AnimatedBox = Animated.createAnimatedComponent(Box);
-
 export default function App() {
-  const POSITION = useRef(
-    new Animated.ValueXY({
-      x: 0,
-      y: 0
+  
+  const panResponder = useRef(PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      onPressIn();
+    },
+    onPanResponderMove: (_,{dx}) => {
+      position.setValue(dx)
+    },
+    onPanResponderRelease: () => {
+      Animated.parallel([
+        onPressOut,
+        Animated.spring(position, {
+          toValue: 0,
+          useNativeDriver: true
+        })
+      ]).start();
+    }
   })).current;
 
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true, // 터치, 드래그 등 프래스 이벤트를 set해준다는 의미
-      onPanResponderMove: (_,{dx, dy}) => {
-        POSITION.setValue({ // POSITION = {x,y} 이거 안됨.
-          x: dx,
-          y: dy
-      })},
-      onPanResponderGrant: () => {
-        POSITION.setOffset({
-          x: POSITION.x._value,
-          y: POSITION.y._value
-        })
-      },
-      onPanResponderRelease: () => {
-        POSITION.flattenOffset(); // offset을 0으로 만들면서 offset값을 POSITION에게 넘겨준다.
-      }
-  })).current
-  
-  const borderRadius = POSITION.y.interpolate({
-    inputRange: [-300, 300], // 대상에서 어떤 값을 갖고 올 것인지 정의
-    outputRange : [100, 0]
-  });
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.Value(0)).current;
+  const onPressIn = () => Animated.spring(scale, {
+    toValue: 0.9,
+    useNativeDriver: true
+  }).start();
 
-  const bgColor = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"]
-  })
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true
+  });
 
   return (
     <Container>
-        <AnimatedBox
-          {...panResponder.panHandlers}
-          style={{transform: [...POSITION.getTranslateTransform()], borderRadius, backgroundColor: bgColor}}
-        />
+      <Card 
+        {...panResponder.panHandlers}
+        style={{
+          transform: [{scale: scale}, {translateX: position}]
+        }}
+      >
+        <Ionicons name='pizza' color="#192a56" size={98}/>
+      </Card>
     </Container>
   );
 }
@@ -74,3 +80,5 @@ export default function App() {
 
 // useRef는 lifeTime동안 object를 지속시켜 준다.
 // 렌더링 시 값이 초기화되는 것을 막아준다.
+
+// [4.12] android에 box-shadow가 적용이 안되면 elevation을 사용한다.
